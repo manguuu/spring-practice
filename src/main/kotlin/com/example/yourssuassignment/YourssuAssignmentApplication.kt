793a16958a1c62f3c99f1badde82b811a894f56c
Mingyu Park <alsrb4298@naver.com>
2022-10-05 00:57:58 +0900

    init

---
+++ src/main/kotlin/com/example/yourssuassignment/YourssuAssignmentApplication.kt
@@ -0,0 +1,11 @@
+package com.example.yourssuassignment
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class YourssuAssignmentApplication
+
+fun main(args: Array<String>) {
+	runApplication<YourssuAssignmentApplication>(*args)
+}
