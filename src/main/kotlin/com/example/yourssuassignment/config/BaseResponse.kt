98b545a7653432718327b7fd61b67445b9dcc285
Mingyu Park <alsrb4298@naver.com>
2022-10-08 01:45:13 +0900

    Config file 수정 안쓰는 BaseResponseStatus 제거, BaseResponse 멤버 수정

--- src/main/kotlin/com/example/yourssuassignment/config/BaseResponse.kt
+++ src/main/kotlin/com/example/yourssuassignment/config/BaseResponse.kt
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder
 import com.example.yourssuassignment.config.BaseResponseStatus.SUCCESS
 
 
-class BaseResponse<T>{
+class BaseResponse{
     //BaseResponse 객체를 사용할때 성공, 실패 경우
     @JsonProperty("isSuccess")
     private val isSuccess: Boolean
@@ -17,16 +17,11 @@ class BaseResponse<T>{
     @JsonProperty("code")
     private val code: Int
 
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @JsonProperty("result")
-    private var result: T? = null
-
     // 요청에 성공한 경우
-    constructor(result: T) {
+    constructor() {
         this.isSuccess = SUCCESS.isSuccess
         this.message = SUCCESS.message
         this.code = SUCCESS.code
-        this.result = result
     }
 
     // 요청에 실패한 경우
